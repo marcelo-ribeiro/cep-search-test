32190ed1235f8f0c0ebbb83cf17905697253081e
Marcelo Ribeiro <marceloribeiromidia@gmail.com>
2022-09-19 17:38:28 -0300

    fix: name changed

--- src/components/CEPSearch/CEPSearchAddress/styled.ts
+++ src/components/CEPSearch/CEPSearchAddress/styled.ts
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const StyledCEPSearchAddress = styled.section`
   display: grid;
   grid-gap: 0.5rem;
-  margin-top: 1.5rem;
   padding: 1.5rem;
   border: 1px solid #fff3;
   border-radius: 0.5rem;
